ba4afb280fc53e43408fb2916f6d88d6945306fb
Joshua <232697825+joshuarobins@users.noreply.github.com>
2026-03-25 16:19:08 -0400

    Updated merged geogjsons to use revised housing dataset

--- cleaning/merging-zcta-geojson-file.py
+++ cleaning/merging-zcta-geojson-file.py
@@ -3,11 +3,11 @@ import pandas as pd
 import os
 
 
-geojson_folder_path = "./../datasets/tl_2023_us_zcta520"
+geojson_folder_path = "./../datasets/geojson-raw"
 geojson_name = "tl_2023_us_zcta520.geojson"
 
 csv_folder_path = "./../datasets/final-usables"
-csv_name = "merged_housing_dataset.csv"
+csv_name = "Revised - Merged Housing Dataset (by ZipCode).csv"
 
 
 geojson_path = os.path.join(geojson_folder_path, geojson_name)
@@ -18,13 +18,13 @@ geo = gpd.read_file(geojson_path)
 csv = pd.read_csv(csv_path)
 
 csv = csv.copy()
-csv['name'] = csv['name'].astype(str).str.zfill(5)
+csv['zip'] = csv['zip'].astype(str).str.strip()
 
 geo = geo.copy()
 geo['GEOID20'] = geo['GEOID20'].astype(str).str.strip()
-geo = geo.merge(csv, left_on='GEOID20', right_on='name', how='inner')
+geo = geo.merge(csv, left_on='GEOID20', right_on='zip', how='inner')
 
-geo = geo.drop(columns=['name'])
+geo = geo.drop(columns=['zip'])
 
 output_file = os.path.join(csv_folder_path, "merged_nyc_zcta.geojson")
 geo.to_file(output_file, driver="GeoJSON")
